9b2fcb352ca9916e55184c7d336d9026ba40989c
Charles Caldwell <charlie@unfalsedesign.com>
2013-02-22 09:25:17 -0600

    Added Excel functionality (first push)

---
+++ lib/monkey_patches/Monkey_Patches.rb
@@ -0,0 +1,12 @@
+module Office
+  module MonkeyPatches
+    require 'date'
+
+    #require_relative 'lib/hash'
+    #require_relative 'lib/date'
+    #require_relative 'lib/time'
+    require_relative 'lib/fixnum'
+    #require_relative 'lib/array'
+    #require_relative 'lib/file'
+  end
+end
